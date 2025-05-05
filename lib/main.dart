2e1ac3b487005ecf59ac01ec2d23c94c66c062be
F6323258 Lucas Pereira Gomes Linard <154244432+lpglinard@users.noreply.github.com>
2025-05-05 20:25:28 -0300

    Add Firebase initialization and dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'firebase_options.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  Future<void> initializeDefault() async {
+    FirebaseApp app = await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    print('Initialized default app $app');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initializeDefault();
+  }
 
   // This widget is the root of your application.
   @override
